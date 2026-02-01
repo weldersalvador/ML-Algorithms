48c2705dbdbbe140bb738c21df355070dfb13ed4
Bárbara C <weldersalvadorvictor12@hotmail.com>
2026-02-01 14:09:56 -0300

    Linear regression

--- MlAlgo.py
+++ MlAlgo.py
@@ -2,9 +2,11 @@ import numpy as np
 
 class Algorithms:
     def __init__(self):
+        self.a = 0
+        self.b = 0
         pass
 
-    def r_calculus(X,Y):
+    def r_calculus(self,X,Y):
         n = len(X)
         nom = n*np.sum(X*Y) - np.sum(X)*np.sum(Y)
         denom = ((n*np.sum(X**2) - np.sum(X)**2)*(n*np.sum(Y**2) - np.sum(Y)**2))**(1/2)
@@ -23,5 +25,22 @@ class Algorithms:
                 corr_matrix[c1][c2] = self.r_calculus(column1,column2)
         corr_matrix = np.array(corr_matrix)
         return corr_matrix
+    
+    def Regression(self,X,Y):
+        X =  np.array(X)
+        Y = np.array(Y)
+        n = len(X)
+        self.b = (n*np.sum(X*Y) - np.sum(X)*np.sum(Y)) / (n*np.sum(X**2) - (np.sum(X))**2)
+        self.a = np.mean(Y) - self.b*np.mean(X)
+        
+    @property
+    def intercept_(self):
+        return self.a
+    @property
+    def coef_(self):
+        return self.b
+        
+
+
     
     
--- main.py
+++ main.py
@@ -3,15 +3,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-obj = Algorithms
+obj = Algorithms()
 
 array = np.array([[1,2,3],
                 [2,4,6],
                 [3,1,5]])
 
-corr_matrix = obj.corr_matrix(obj,array)
+corr_matrix = obj.corr_matrix(array)
 
 print(corr_matrix)
+
+print()
+
+X = np.array([1,2,5,2,5,7,2,3,5])
+
+Y = np.array([3,1,5,7,3,2,6,8,3])
+
+obj.Regression(X,Y)
+print(f"Coeficient: {obj.coef_}\nIntercept: {obj.intercept_}")
+
+y_pred = obj.coef_*X + obj.intercept_
+
+plt.scatter(x = X, y = Y)
+plt.plot(X,y_pred)
+plt.show()
 
 
 
